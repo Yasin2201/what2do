6e864d2dcf2038ab99192c10c757b6d2e785d7aa
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-05 16:27:43 +0000

    install and use cors

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
@@ -10,6 +11,7 @@ const prisma = new PrismaClient()
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
