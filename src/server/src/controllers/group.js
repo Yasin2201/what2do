1a41c64998155ee1629a4ec074d225ea8f2616a4
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-06 14:03:36 +0000

    create group create and delete controllers

--- src/server/src/controllers/group.js
+++ src/server/src/controllers/group.js
@@ -1,11 +1,36 @@
 const db = require("../db");
 
 exports.create = async (req, res, next) => {
+  const { name } = req.body
 
+  if (!name) {
+    return res
+      .status(422)
+      .json({ error: "Name must be provided" });
+  }
+
+  const group = await db.group.create({
+    data: {
+      name,
+    },
+  });
+
+  return res.json({group});
 };
 
 exports.delete = async (req, res, next) => {
+  const { id } = req.params
 
+  try {
+    const deletedGroup = await db.group.delete({
+      where: {
+        id
+      },
+    })
+    return res.json({deletedGroup});
+  } catch (error) {
+    res.status(404).json({error})
+  }
 };
 
 exports.update = async (req, res, next) => {
