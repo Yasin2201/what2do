7066236d1ffe84b1e1ef6db16362c8fc3b5caab5
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-06 13:22:42 +0000

    create group controller

---
+++ src/server/src/controllers/group.js
@@ -0,0 +1,21 @@
+const db = require("../db");
+
+exports.create = async (req, res, next) => {
+
+};
+
+exports.delete = async (req, res, next) => {
+
+};
+
+exports.update = async (req, res, next) => {
+
+};
+
+exports.getAll = async (req, res, next) => {
+
+};
+
+exports.getOne = async (req, res, next) => {
+
+};
