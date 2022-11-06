00c8174e30152a2787ae2cea3693db158180cef8
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-06 14:02:55 +0000

    add group router to router

--- src/server/src/router.js
+++ src/server/src/router.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 var authRouter = require('./routes/authentication')
+var groupRouter = require('./routes/group')
 
 router.use("/auth", authRouter);
+router.use(groupRouter);
 
 module.exports = router;
