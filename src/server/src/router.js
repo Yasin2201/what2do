6168cc5194f03fbd650ac39d5f8b9a1c9b24e4b8
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-06 15:52:15 +0000

    add activity routes to router

--- src/server/src/router.js
+++ src/server/src/router.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 var authRouter = require('./routes/authentication')
 var groupRouter = require('./routes/group')
+const activityRouter = require('./routes/activity')
 
 router.use("/auth", authRouter);
 router.use(groupRouter);
+router.use(activityRouter);
 
 module.exports = router;
