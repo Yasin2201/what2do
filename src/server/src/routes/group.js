bcf97a872776947d4b2550ab6171b728798fcc5f
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-06 17:12:43 +0000

    add join group route

--- src/server/src/routes/group.js
+++ src/server/src/routes/group.js
@@ -5,5 +5,6 @@ const Group = require("../controllers/group");
 router.post("/group", Group.create);
 router.delete("/group/:id", Group.delete);
 router.put("/group/:id", Group.update);
+router.put("/group/join/:id", Group.joinGroup);
 
 module.exports = router
