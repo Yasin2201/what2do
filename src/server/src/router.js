c2892cda0be88711caca79c7a101cb4f23c1e9da
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-09 13:45:45 +0000

    setup vote routes

--- src/server/src/router.js
+++ src/server/src/router.js
@@ -4,9 +4,11 @@ var router = express.Router();
 var authRouter = require('./routes/authentication')
 var groupRouter = require('./routes/group')
 const activityRouter = require('./routes/activity')
+const voteRouter = require("./routes/vote")
 
 router.use("/auth", authRouter);
 router.use(groupRouter);
 router.use(activityRouter);
+router.use(voteRouter);
 
 module.exports = router;
