9867debf9ede24b4153eb5b24a8112f93a570d59
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-16 23:44:04 +0000

    add tailwindcss styling to form

--- src/client/src/features/auth/components/LoginForm.js
+++ src/client/src/features/auth/components/LoginForm.js
@@ -1,44 +1,78 @@
-import { useAuth } from "@/lib/auth"
+import { useLogin } from "@/hooks/useLogin"
 import { useState } from "react"
 
-export const LoginForm = ({onSuccess}) => {
-  const { isLoading, loginFn, user } = useAuth()
+export const LoginForm = () => {
+  const { login, error, isLoading } = useLogin()
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-
-    const data = {
-      username,
-      password
-    }
-
-    loginFn(data)
-  }
-
-  if (user) {
-    onSuccess()
+    await login(username, password)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login Form</h1>
-      <div>
-        <label htmlFor="username">Username: </label>
-        <input name="username" type="text" value={username} onChange={(e) => {setUsername(e.target.value)}} />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input name="password" type="text" value={password} onChange={(e) => {setPassword(e.target.value)}} />
-      </div>
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+        <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label htmlFor="username" className="sr-only">
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => {setUsername(e.target.value)}}
+              />
+            </div>
+          </div>
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="text"
+                autoComplete="password"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {setPassword(e.target.value)}}
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              disabled={(isLoading ? true : false)} 
+              type="submit"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white enabled:hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
       {
-        isLoading ? 
-        <button disbaled="true" type="submit">Login</button>
-        :
-        <button type="submit">Login</button>
+        error && 
+        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+          <strong className="font-bold">Error: </strong>
+          <span className="block sm:inline">{error}</span>
+        </div>
       }
-    </form>
+      </div>
+    </div>
   )
 }
