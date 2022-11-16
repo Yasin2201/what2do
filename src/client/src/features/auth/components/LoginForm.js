2abd12d406e9d01a5fef71c93e8b2f77f266f4d0
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-16 16:04:43 +0000

    create loginForm component

---
+++ src/client/src/features/auth/components/LoginForm.js
@@ -0,0 +1,44 @@
+import { useAuth } from "@/lib/auth"
+import { useState } from "react"
+
+export const LoginForm = ({onSuccess}) => {
+  const { isLoading, loginFn, user } = useAuth()
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const data = {
+      username,
+      password
+    }
+
+    loginFn(data)
+  }
+
+  if (user) {
+    onSuccess()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Login Form</h1>
+      <div>
+        <label htmlFor="username">Username: </label>
+        <input name="username" type="text" value={username} onChange={(e) => {setUsername(e.target.value)}} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input name="password" type="text" value={password} onChange={(e) => {setPassword(e.target.value)}} />
+      </div>
+      {
+        isLoading ? 
+        <button disbaled="true" type="submit">Login</button>
+        :
+        <button type="submit">Login</button>
+      }
+    </form>
+  )
+}
