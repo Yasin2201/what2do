33b8aed5fbaa73889ff5394d49756e2b3af054e3
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-16 16:05:22 +0000

    refactor login and register routes

--- src/client/src/features/auth/routes/Register.js
+++ src/client/src/features/auth/routes/Register.js
@@ -1,16 +1,10 @@
-export const Register = () => {
+import { useNavigate } from 'react-router-dom';
+import { RegisterForm } from '../components/RegisterForm';
+
+export const Login = () => {
+  const navigate = useNavigate();
+
   return (
-    <form>
-      <h1>Register Form</h1>
-      <div>
-        <label htmlFor="username">Username: </label>
-        <input name="username" type="text" />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input name="password" type="text" />
-      </div>
-      <button>Register</button>
-    </form>
-  )
-}
+      <RegisterForm onSuccess={() => navigate('/dash')} />
+  );
+};
