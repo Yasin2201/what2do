23ba31e541e80b2fccd146f1ab35595dcf1a1af9
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-17 00:37:50 +0000

    use passport local strategy in route to use custom errors

--- src/server/src/controllers/authentication.js
+++ src/server/src/controllers/authentication.js
@@ -1,14 +1,25 @@
 const jwt = require("jsonwebtoken");
 const { secret } = require("../config");
 const db = require("../db");
+const passport = require("passport");
+require("../services/passport");
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
   return jwt.sign({ sub: user.id, iat: timestamp }, secret);
 }
 
-exports.login = function (req, res) {
-  return res.send({ token: tokenForUser(req.user) });
+exports.login = function (req, res, next) {
+  passport.authenticate("local", function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { 
+        res.status(401);
+        res.send(info.message);
+        return;
+    }
+    const {id, username} = user
+    return res.send({ token: tokenForUser(user), user: {id, username} });
+  })(req, res, next);
 };
 
 exports.signup = async (req, res, next) => {
@@ -18,7 +29,7 @@ exports.signup = async (req, res, next) => {
   if (!username || !password) {
     return res
       .status(422)
-      .json({ error: "Username and password must be provided" });
+      .send({ error: "Username and password must be provided" });
   }
 
   const existingUser = await db.user.findFirst({
@@ -28,7 +39,7 @@ exports.signup = async (req, res, next) => {
   });
 
   if (existingUser) {
-    return res.status(422).json({ error: "Username is already in use..." });
+    return res.status(422).send({ error: "Username is already in use" });
   }
 
   const user = await db.user.create({
@@ -38,5 +49,5 @@ exports.signup = async (req, res, next) => {
     },
   });
 
-  return res.json({token: tokenForUser(user)});
+  return res.send({ token: tokenForUser(user), user});
 };
