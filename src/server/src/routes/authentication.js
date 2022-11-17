437bc57b4fcbc565fa8e96edc7c3aa6eacdacc0e
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-17 00:38:19 +0000

    update auth routes

--- src/server/src/routes/authentication.js
+++ src/server/src/routes/authentication.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Authentication = require("../controllers/authentication");
-const passport = require("passport");
-require("../services/passport");
 
-const requireSignIn = passport.authenticate("local", { session: false });
-
-router.post("/login", requireSignIn, Authentication.login);
+router.post("/login", Authentication.login);
 router.post("/signup", Authentication.signup);
 
 module.exports = router
