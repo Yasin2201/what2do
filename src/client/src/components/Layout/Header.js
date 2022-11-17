f4e32a44904d4e1e45f0377b318ee386d90cb3c5
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-17 17:43:24 +0000

    add logout func to logout btn

--- src/client/src/components/Layout/Header.js
+++ src/client/src/components/Layout/Header.js
@@ -1,8 +1,10 @@
 import { useAuthContext } from "@/hooks/useAuthContext"
+import { useLogout } from "@/hooks/useLogout"
 
 export const Header = () => {
   const {user} = useAuthContext()
-
+  const {logout} = useLogout()
+  
   return (
     <div className="mx-auto px-2 sm:px-6 lg:px-10 bg-gray-800">
       <div className="relative flex h-16 items-center justify-between">
@@ -11,11 +13,11 @@ export const Header = () => {
             <div className="px-4">
               <p className="text-white font-bold">{user.username}</p>
             </div>
-            <a href="/" className="px-4">
-              <button className="text-white font-bold py-1 px-2 rounded border-2 border-gray-100 hover:bg-gray-100 hover:text-gray-800">
+            <div className="px-4">
+              <button onClick={logout} className="text-white font-bold py-1 px-2 rounded border-2 border-gray-100 hover:bg-gray-100 hover:text-gray-800">
                 Sign Out
               </button>
-            </a>
+            </div>
           </div>
         </div>
       </div>
