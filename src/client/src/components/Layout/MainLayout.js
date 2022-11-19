83e342b6aa4f8112aa756f854e5dbb676cc4ba9b
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-19 17:13:37 +0000

    npm i react-icons

--- src/client/src/components/Layout/MainLayout.js
+++ src/client/src/components/Layout/MainLayout.js
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "./Header"
+import Sidebar from "./Sidebar"
 
 export const MainLayout = () => {
   return (
-    <div className="h-screen overflow-hidden bg-gray-100">
+    <div className="flex w-100 h-full">
+      <Sidebar />
+      <div id="home" className="flex flex-col w-full">
       <Header />
       <Outlet />
+      </div>
     </div>
   )
 }
