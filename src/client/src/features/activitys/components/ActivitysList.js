8626850e0036fe4923392e913328138758c37f33
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-19 20:18:28 +0000

    create activitys list component to list all users activities

---
+++ src/client/src/features/activitys/components/ActivitysList.js
@@ -0,0 +1,32 @@
+import { useActivitys } from "../api/getActivitys";
+
+export const ActivitysList = () => {
+  const activitysQuery = useActivitys()
+
+  if (activitysQuery.isLoading) {
+    return (
+      <div className="w-full h-48 flex justify-center items-center">
+        Loading....
+      </div>
+    )
+  }
+
+  if (!activitysQuery.data) return null;
+
+  const { data } = activitysQuery.data
+
+  return (
+    <div>
+      {data.allActivities.map((activity) => {
+        return (
+          <div key={activity.activity_id} className="border border-black p-2 my-2 ">
+            <p>Name: {activity.name}</p>
+            <p>Group: {activity.group_name}</p>
+            <p>Status: {activity.status}</p>
+            <p>Created By: {activity.username}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
