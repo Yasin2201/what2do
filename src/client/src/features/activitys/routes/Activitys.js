1bae3c0074ded20d90b1f2843db608d904cc15e1
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-19 17:34:04 +0000

    create activitys  route

---
+++ src/client/src/features/activitys/routes/Activitys.js
@@ -0,0 +1,8 @@
+import { ContentLayout } from "@/components/Layout/ContentLayout"
+
+export const Activitys = () => {
+  return (
+    <ContentLayout title="Activities">
+    </ContentLayout>
+  )
+}
