ca6db8357adbcf21dbe42f0dbd577a6fc9ab5215
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-19 20:41:42 +0000

    pass activit id to queryfn within obj

--- src/client/src/features/activitys/api/getActivity.js
+++ src/client/src/features/activitys/api/getActivity.js
@@ -2,12 +2,12 @@ import { axios } from "@/lib/axios";
 import { useQuery } from "react-query";
 
 export const getActivity = ({activityId}) => {
-  return axios.get(`/activitys/${activityId}`)
+  return axios.get(`/activity/${activityId}`)
 }
 
 export const useActivity = (activityId) => {
   return useQuery({
-    queryKey: ['activity', activityId],
-    queryFn: () => getActivity(activityId)
+    queryKey: ["activity", activityId],
+    queryFn: () => getActivity({activityId})
   })
 }
