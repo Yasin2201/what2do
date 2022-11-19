22b97760c508f05e6abf91a2bec31a0cb33a516a
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-19 18:07:57 +0000

    add GroupsList component to Groups route

--- src/client/src/features/groups/routes/Groups.js
+++ src/client/src/features/groups/routes/Groups.js
@@ -1,8 +1,12 @@
 import { ContentLayout } from "@/components/Layout/ContentLayout"
+import { GroupsList } from "../components/GroupsList"
 
 export const Groups = () => {
   return (
     <ContentLayout title="Groups">
+      <div className="mt-4">
+        <GroupsList />
+      </div>
     </ContentLayout>
   )
 }
