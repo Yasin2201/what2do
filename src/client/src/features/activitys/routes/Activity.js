24242455b4bf120b32a82e79101a1bb9795bcee2
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-19 20:26:01 +0000

    create Activity route component

---
+++ src/client/src/features/activitys/routes/Activity.js
@@ -0,0 +1,11 @@
+import { ContentLayout } from "@/components/Layout/ContentLayout"
+
+export const Activity = () => {
+  return (
+    <ContentLayout title="Activity">
+      <div className="mt-4">
+        Hello
+      </div>
+    </ContentLayout>
+  )
+}
