8756d8a32cdb9431f39016ab1144db1c52bc3dcf
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-19 20:19:00 +0000

    render activitysList in Activitys route

--- src/client/src/features/activitys/routes/Activitys.js
+++ src/client/src/features/activitys/routes/Activitys.js
@@ -1,8 +1,12 @@
 import { ContentLayout } from "@/components/Layout/ContentLayout"
+import { ActivitysList } from "../components/ActivitysList"
 
 export const Activitys = () => {
   return (
     <ContentLayout title="Activities">
+      <div className="mt-4">
+        <ActivitysList />
+      </div>
     </ContentLayout>
   )
 }
