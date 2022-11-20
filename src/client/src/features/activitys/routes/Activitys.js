e581ad651a6042025a73f872e60481170db30a46
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-20 20:39:33 +0000

    render create activiy component:

--- src/client/src/features/activitys/routes/Activitys.js
+++ src/client/src/features/activitys/routes/Activitys.js
@@ -1,9 +1,11 @@
 import { ContentLayout } from "@/components/Layout/ContentLayout"
 import { ActivitysList } from "../components/ActivitysList"
+import { CreateActivity } from "../components/CreateActivity"
 
 export const Activitys = () => {
   return (
     <ContentLayout title="Activities">
+      <CreateActivity />
       <div className="mt-4">
         <ActivitysList />
       </div>
