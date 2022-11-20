bf5168ebdad9cd563f82b28af588fb77170f9196
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-20 20:38:08 +0000

    createActivity api call and mutation

---
+++ src/client/src/features/activitys/api/createActivity.js
@@ -0,0 +1,20 @@
+import { axios } from "@/lib/axios";
+import { queryClient } from "@/lib/react-query";
+import { useMutation } from "react-query";
+
+export const createActivity = (data) => {
+  return axios.post(`/activitys`, data);
+};
+
+export const useCreateActivity  = () => {
+  return useMutation({
+    onSuccess: () => {
+      queryClient.invalidateQueries('activitys');
+    },
+    onError: () => {
+      throw Error("Something went wrong")
+    },
+    mutationFn: createActivity,
+  });
+};
+
