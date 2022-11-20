fe7b1b128a309ba2a50aa71cebdef489c49a0387
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-20 12:49:16 +0000

    render CreateGroup in Groups route

--- src/client/src/features/groups/routes/Groups.js
+++ src/client/src/features/groups/routes/Groups.js
@@ -1,9 +1,11 @@
 import { ContentLayout } from "@/components/Layout/ContentLayout"
+import { CreateGroup } from "../components/CreateGroup"
 import { GroupsList } from "../components/GroupsList"
 
 export const Groups = () => {
   return (
     <ContentLayout title="Groups">
+      <CreateGroup />
       <div className="mt-4">
         <GroupsList />
       </div>
