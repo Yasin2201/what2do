2f60b3df3dac4df968b91e22c1ff83b5daf30620
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-20 17:24:37 +0000

    render delete Modal when deleting group

--- src/client/src/features/groups/components/DeleteGroup.js
+++ src/client/src/features/groups/components/DeleteGroup.js
@@ -1,8 +1,11 @@
+import { Modal } from "@/components/Elements/Modal"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useDeleteGroup } from "../api/deleteGroup"
 
-export const DeleteGroup = ({id, isAdmin}) => {
+export const DeleteGroup = ({id, isAdmin, group}) => {
   const deleteGroupMutation = useDeleteGroup()
+  const [showModal, setShowModal] = useState(false)
   const navigate = useNavigate()
 
   const handleDelete = async (e) => {
@@ -20,10 +23,31 @@ export const DeleteGroup = ({id, isAdmin}) => {
 
   return (
     isAdmin && 
-    <div className="flex justify-end">
-      <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-5 rounded" type="button" onClick={handleDelete} >
-        Delete Group
-      </button>
-    </div>
+    <>
+      <div className="flex justify-end">
+        <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-5 rounded" type="button" onClick={() => setShowModal(true)} >
+          Delete Group
+        </button>
+      </div>
+      {showModal ? (
+      <Modal title="Delete Group">
+        <div className="flex flex-col p-5 w-full text-center">
+          <p className="text-gray-800 text-semibold">Are you sure you want to delete group: {group.name}</p>
+          <div className="flex justify-evenly pt-5">
+            <button
+                className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-5 rounded"
+                type="button"
+                onClick={() => setShowModal(false)}
+                >
+                Cancel
+            </button>
+            <button className="bg-red-500 hover:bg-red-60 text-white font-semibold py-1 px-5 rounded" type="button" onClick={handleDelete}>
+                Delete
+            </button>
+          </div>
+        </div>
+      </Modal>
+      ) : null}
+    </>
   )
 }
