e8c84191c560a92dd097d5a76d6dc440f1f83c8b
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-23 16:06:50 +0000

    add error to modal

--- src/client/src/components/Elements/Modal.js
+++ src/client/src/components/Elements/Modal.js
@@ -1,4 +1,4 @@
-export const Modal = ({children, title}) => {
+export const Modal = ({children, title, error}) => {
   return (
     <>
       <div className="justify-center mt-40 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
@@ -6,6 +6,13 @@ export const Modal = ({children, title}) => {
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col items-center w-full bg-white outline-none focus:outline-none">
             <h2 className="text-2xl font-semibold text-gray-800 m-4">{title}</h2>
             {children}
+            {
+              error &&
+                <div className="bg-red-100 border border-red-400 text-red-700 p-1 m-1 rounded relative" role="alert">
+                  <strong className="font-bold">Error: </strong>
+                  <span className="block sm:inline">{error}</span>
+                </div>
+            }
           </div>
         </div>
       </div>
