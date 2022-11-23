f4d7f2911a7225f28b6a7f8bf66a785ad31faa93
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-23 18:01:37 +0000

    remove padding

--- src/client/src/components/Elements/Modal.js
+++ src/client/src/components/Elements/Modal.js
@@ -3,7 +3,7 @@ export const Modal = ({children, title, error}) => {
     <>
       <div className="justify-center mt-40 flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col items-center w-full bg-white outline-none focus:outline-none">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col items-center w-full p-5 bg-white outline-none focus:outline-none">
             <h2 className="text-2xl font-semibold text-gray-800 m-4">{title}</h2>
             {children}
             {
