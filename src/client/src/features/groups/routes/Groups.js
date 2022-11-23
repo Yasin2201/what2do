ff3eba5b8820873adcaf8cdd2dc6327eb5661197
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-23 16:06:29 +0000

    render join group component:

--- src/client/src/features/groups/routes/Groups.js
+++ src/client/src/features/groups/routes/Groups.js
@@ -1,11 +1,15 @@
 import { ContentLayout } from "@/components/Layout/ContentLayout"
 import { CreateGroup } from "../components/CreateGroup"
 import { GroupsList } from "../components/GroupsList"
+import { JoinGroup } from "../components/JoinGroup"
 
 export const Groups = () => {
   return (
     <ContentLayout title="Groups">
-      <CreateGroup />
+      <div className="flex justify-end">
+        <JoinGroup />
+        <CreateGroup />
+      </div>
       <div className="mt-4">
         <GroupsList />
       </div>
