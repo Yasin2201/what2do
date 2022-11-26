d26fe5755be81fd41ee17e1017800e8b691ef247
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-26 18:17:17 +0000

    render current activity details in completed component

--- src/client/src/features/activitys/components/status/Completed.js
+++ src/client/src/features/activitys/components/status/Completed.js
@@ -1,7 +1,38 @@
+import { Link, useParams } from "react-router-dom"
+import { useActivity } from "../../api/getActivity"
+import { ActivityUsers } from "../ActivityUsers"
+import { DeleteActivity } from "../DeleteActivity"
+
 export const Completed = () => {
+  const { id } = useParams()
+  const activityQuery = useActivity(id)
+
+  if (activityQuery.isLoading) {
+    return (
+      <div className="w-full h-48 flex justify-center items-center">
+        Loading....
+      </div>
+    )
+  }
+
+  if (!activityQuery.data) return null
+
+  const { activity } = activityQuery.data.data
+  const status = activity.status.toLowerCase()
+
   return (
     <div>
-      Completed
+      <DeleteActivity id={activity.id} isAdmin={activity.isAdmin} activity={activity}/>
+      <Link to={`/activitys/${status}`} className="underline">Back</Link>
+      <p>
+        group: {activity.name}
+      </p>
+      <p>
+        status: {activity.status}
+      </p>
+      <div>
+        <ActivityUsers users={activity.group.users}/>
+      </div>
     </div>
   )
 }
