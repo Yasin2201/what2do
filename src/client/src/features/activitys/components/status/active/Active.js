6d7dc0b2d72f9159c27cc74dee33317441651cee
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-28 13:48:35 +0000

    add active folder and move Active component

---
+++ src/client/src/features/activitys/components/status/active/Active.js
@@ -0,0 +1,38 @@
+import { Link, useParams } from "react-router-dom"
+import { useActivity } from "../../../api/getActivity"
+import { ActivityUsers } from "../../ActivityUsers"
+import { DeleteActivity } from "../../DeleteActivity"
+
+export const Active = () => {
+  const { id } = useParams()
+  const activityQuery = useActivity(id)
+
+  if (activityQuery.isLoading) {
+    return (
+      <div className="w-full h-48 flex justify-center items-center">
+        Loading....
+      </div>
+    )
+  }
+
+  if (!activityQuery.data) return null
+
+  const { activity } = activityQuery.data.data
+  const status = activity.status.toLowerCase()
+
+  return (
+    <div>
+      <DeleteActivity id={activity.id} isAdmin={activity.isAdmin} activity={activity}/>
+      <Link to={`/activitys/${status}`} className="underline">Back</Link>
+      <p>
+        group: {activity.name}
+      </p>
+      <p>
+        status: {activity.status}
+      </p>
+      <div>
+        <ActivityUsers users={activity.group.users}/>
+      </div>
+    </div>
+  )
+}
