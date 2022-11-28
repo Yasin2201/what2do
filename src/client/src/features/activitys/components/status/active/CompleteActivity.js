3fb34577e54259ec835b0857aebe601ed2f2c776
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-28 16:08:28 +0000

    fn to handleUpdate and navigate away on sucess

--- src/client/src/features/activitys/components/status/active/CompleteActivity.js
+++ src/client/src/features/activitys/components/status/active/CompleteActivity.js
@@ -1,14 +1,24 @@
 import { Modal } from "@/components/Elements/Modal"
+import { useCompleteActivity } from "@/features/activitys/api/completeActivity"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-export const CompleteActivity = ({isAdmin}) => {
+export const CompleteActivity = ({id, isAdmin}) => {
   const [showModal, setShowModal] = useState(false)
+  const completeActivityMutation = useCompleteActivity()
   const navigate = useNavigate()
 
   const handleUpdate = async (e) => {
     e.preventDefault()
-    alert("Completed")
+    await completeActivityMutation.mutateAsync(id)
+    .then(res => {
+      if (res.status === 200) {
+        return navigate("/activitys/completed")
+      }
+    })
+    .catch(err => {
+      throw Error(err)
+    })
   }
 
   return (
