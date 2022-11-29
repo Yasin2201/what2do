0b8655a2693935e3773ee5a96da5632c582e3daf
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-29 17:42:03 +0000

    add placeRouter

--- src/server/src/router.js
+++ src/server/src/router.js
@@ -6,11 +6,13 @@ const groupRouter = require('./routes/group')
 const activityRouter = require('./routes/activity')
 const voteRouter = require("./routes/vote")
 const userRouter = require("./routes/user")
+const placeRouter = require("./routes/place")
 
 router.use("/auth", authRouter);
 router.use(groupRouter);
 router.use(activityRouter);
 router.use(voteRouter);
 router.use(userRouter);
+router.use(placeRouter);
 
 module.exports = router;
