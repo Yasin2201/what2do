6a49959a5bfbec75b80449136e7edfec2933a444
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-30 17:52:09 +0000

    create activityTypes to get data for places with types

---
+++ src/server/src/utils/activityTypes.js
@@ -0,0 +1,33 @@
+exports.activityTypes = {
+  test: [
+    "movie_theater",
+    "amusement_park"
+  ],
+  funActivities: [
+    "amusement_park",  
+    "bowling_alley",
+    "park",
+    "shopping_mall",
+    "tourist_attraction",
+    "zoo",
+  ],
+  relaxingDay: [
+    "aquarium",
+    "art_gallery",
+    "movie_theater",
+    "museum",
+    "spa",
+  ],
+  nightLife: [
+    "bowling_alley",
+    "casino",
+    "night_club",
+    "bar",
+  ],
+  food: [
+    "bar",
+    "cafe",
+    "meal_takeaway",
+    "restaurant",
+  ]
+}
