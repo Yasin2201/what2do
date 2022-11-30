a77d0dfe71d3d2aba235fdf068bf09df7e2e1c91
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-30 17:50:21 +0000

    remove createactivity component

--- src/client/src/features/activitys/routes/Activitys.js
+++ src/client/src/features/activitys/routes/Activitys.js
@@ -1,11 +1,9 @@
 import { ContentLayout } from "@/components/Layout/ContentLayout"
-import { CreateActivity } from "../components/CreateActivity"
 import { ActivitysList } from "../components/ActivitysList"
 
 export const Activitys = () => {
   return (
     <ContentLayout title="Activities">
-      <CreateActivity />
       <div className="mt-4 h-[75vh] overflow-scroll">
         <ActivitysList />
       </div>
