8c265745b0943b8d42ae83b1a27c13bd2e3eca77
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-30 17:47:57 +0000

    add disance radiuses

---
+++ src/client/src/components/Elements/Dropdown/utils/distanceRadius.js
@@ -0,0 +1,22 @@
+export const distanceRadius = [
+  {
+    id: 1,
+    meter: 1609,
+    miles: "< 1 mile"
+  },
+  {
+    id: 2,
+    meter: 8046,
+    miles: "< 5 miles"
+  },
+  {
+    id: 3,
+    meter: 16093,
+    miles: "< 10 miles"
+  },
+  {
+    id: 4,
+    meter: 24140,
+    miles: "< 15 miles"
+  },
+]
