82be348a0695128329c940717ee723f33945f438
Yasin Amin <yasin.amin@hotmail.co.uk>
2022-11-30 17:49:22 +0000

    refactor

--- src/client/src/components/Elements/Dropdown/GroupDropdown.js
+++ src/client/src/components/Elements/Dropdown/GroupDropdown.js
@@ -16,20 +16,18 @@ export const GroupDropdown = () => {
   const { allGroups } = groupsQuery.data.data
 
   return (
-    <>
-      <div className="relative w-full lg:max-w-sm px-5 pb-5">
-        <label className="text-gray-700 text-lg font-medium">Group</label>
-        <select id="groupId" required defaultValue={""} className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
-          <option value="" disabled>Select Group</option>
-          {
-            allGroups.map((group => {
-              return (
-                <option key={group.groupId} value={group.groupId}>{group.group.name}</option>
-              )
-            }))
-          }
-        </select>
-      </div>
-    </>
+    <div className="mt-5">
+      <label className="text-gray-700 text-md font-medium">Group</label>
+      <select id="groupId" required defaultValue={""} className="w-full p-1.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none focus:border-indigo-600">
+        <option value="" disabled>Select Group</option>
+        {
+          allGroups.map((group => {
+            return (
+              <option key={group.groupId} value={group.groupId}>{group.group.name}</option>
+            )
+          }))
+        }
+      </select>
+    </div>
   )
 }
